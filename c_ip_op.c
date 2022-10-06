d751aaa0605fcf8070bc7ab50cf9cf2e6a12b7a0
M SIVANANTHAN <113927784+MSivananthan@users.noreply.github.com>
2022-10-06 19:15:16 +0530

    Add files via upload

---
+++ c_ip_op.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+int testInteger = 5;
+printf("Number = %d", testInteger);
+return 0;
+}
+#include <stdio.h>
+int main()
+{
+float number1 = 13.5;
+double number2 = 12.4;
+printf("number1 = %f\n", number1);
+printf("number2 = %lf", number2);
+return 0;
+}
+#include <stdio.h>
+int main()
+{
+char chr = 'a';
+printf("character = %c", chr);
+return 0;
+}
