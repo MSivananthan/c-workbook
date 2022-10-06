68d3dc88fb9a0ba1a0fc462e915a6962116aec7b
M SIVANANTHAN <113927784+MSivananthan@users.noreply.github.com>
2022-10-06 19:15:00 +0530

    Add files via upload

---
+++ arithmetic_operators.c
@@ -0,0 +1,17 @@
+// Working of arithmetic operators
+#include <stdio.h>
+int main()
+{
+int a = 8,b = 5, c;
+c = a+b;
+printf("a+b = %d \n",c);
+c = a-b;
+printf("a-b = %d \n",c);
+c = a*b;
+printf("a*b = %d \n",c);
+c = a/b;
+printf("a/b = %d \n",c);
+c = a%b;
+printf("Remainder when a divided by b = %d \n",c);
+return 0;
+}
